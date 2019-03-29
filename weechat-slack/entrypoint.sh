0c89d724835e4c38928bda0945b24e087672109b
David James <djames@spreedly.com>
2019-03-29 11:10:40 -0400

    Add core scripts with script command.

--- weechat-slack/entrypoint.sh
+++ weechat-slack/entrypoint.sh
@@ -2,8 +2,9 @@
 
 if [[ -z ${SLACK_TOKEN} ]]; then echo "Provide your Slack token."; exit; fi
 
-weechat -r "/python load /tmp/autosort.py; \
+weechat -r "/script install autosort.py; \
+            /script install go.py; \
+            /script install vimode.py; \
             /python load /tmp/wee_slack.py; \
-            /python load /tmp/weemoji.py; \
             /set plugins.var.python.slack.slack_api_token \"${SLACK_TOKEN}\";\
             /python reload slack;"
