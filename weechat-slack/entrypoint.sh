64bb84d99ee614e929bafb0f26560d744fdb2f46
David James <djames@spreedly.com>
2019-04-03 11:16:39 -0400

    Got have the ¯\_(ツ)_/¯

--- weechat-slack/entrypoint.sh
+++ weechat-slack/entrypoint.sh
@@ -5,6 +5,9 @@ if [[ -z ${SLACK_TOKEN} ]]; then echo "Provide your Slack token."; exit; fi
 weechat -r "/script install autosort.py; \
             /script install go.py; \
             /script install vimode.py; \
+            /python load /tmp/shrug.py; \
             /python load /tmp/wee_slack.py; \
-            /set plugins.var.python.slack.slack_api_token \"${SLACK_TOKEN}\";\
-            /python reload slack;"
+            /set plugins.var.python.slack.slack_api_token \"${SLACK_TOKEN}\"; \
+            /python reload slack; \
+            /mouse enable;"
+
